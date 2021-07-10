32613f731cce97a74f0ddc351e43af30dacd105b
Dennis Nayandin <nayandindennis@yahoo.com>
2021-07-10 01:43:01 -0400

    signin / signout done

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,17 @@ import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
 const App = () => {
-
+  const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
         <Navbar />
+        <Switch>
+          <Route path="/" exact component={() => <Redirect to="/calender" />} />
+          <Route path="/calender" exact component={Home} />
+          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/calender" />)} />
+        </Switch>
       </Container>
     </BrowserRouter>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { AppBar, Typography, Toolbar, Icon, Button, Avatar } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
+import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 
 
 const Navbar = () => {
-    const user = null;
-
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const history = useHistory();
     const classes = useStyles();
 
     const logout = () => {
-        console.log("Logging out");
-    }
+        dispatch({ type: actionType.LOGOUT });
+
+        history.push('/auth');
+
+        setUser(null);
+    };
+
+    useEffect(() => {
+        const token = user?.token;
+
+        if (token) {
+        const decodedToken = decode(token);
+
+        if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -23,21 +44,19 @@ const Navbar = () => {
                     <MonetizationOnIcon className={classes.iconName} />
                 </div>
             </Link>
-            
             <Toolbar>
                 {user?.result ? (
-                    <div>
-                        <Avatar></Avatar>
-                        <Typography></Typography>
-                        <Button variant="contained" color="secondary" onClick={logout}>Logout</Button>
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+                        <Button className={classes.logout} variant="contained" color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
                 )}
-                
             </Toolbar>
         </AppBar>
-    )
-}
+    );
+};
 
 export default Navbar;
