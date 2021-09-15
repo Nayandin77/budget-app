3ba7c90624d4539c8fdc00aa446b5b2fa9da6c19
Dennis Nayandin <nayandindennis@yahoo.com>
2021-09-15 01:44:29 -0400

    working add loading and adding items to details

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,74 +1,92 @@
-import React from 'react';
-import { Container, Card, Typography, List, ListItem, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Card, Typography, List, ListItem, Button, Input } from '@material-ui/core';
+
+const testData = [
+    {
+        id: 1,
+        name: "Groceries",
+        items: {
+            0: "test 0",
+            1: "test 1",
+            2: "test 2",
+        },
+    },
+    {
+        id: 2,
+        name: "Sports",
+        items: {
+            0: "test 0",
+            1: "test 1",
+            2: "test 2",
+        },
+    },
+    {
+        id: 3,
+        name: "Miscellaneous",
+        items: {
+            0: "test 0",
+            1: "test 1",
+            2: "test 2",
+        }
+    },
+    {
+        id: 4,
+        name: "House Hold Items",
+        items: {
+            0: "test 0",
+            1: "test 1",
+            2: "test 2",
+        }
+    },
+];
 
 const Details = () => {
-    
-    const testData = {
-        '0' : {
-            'name': "Groceries",
-            'items': {
-                0: "test 0",
-                1: "test 1",
-                2: "test 2",
-            },
-        },
-        '1' : {
-            'name': "Sports",
-            'items': {
-                0: "test 0",
-                1: "test 1",
-                2: "test 2",
-            },
-        },
-        '2' : {
-            'name': "Miscellaneous",
-            'items': {
-                0: "test 0",
-                1: "test 1",
-                2: "test 2",
-            }
-        },
-        '3' : {
-            'name': "House Hold Items",
-            'items': {
-                0: "test 0",
-                1: "test 1",
-                2: "test 2",
-            }
-        },
-    }
-    
+    const [details, setDetails] = useState(testData);
+    const [detail, setDetail] = useState('');
 
-    const parseData = () => { 
-        const array = [];    
-        for (const [key, value] of Object.entries(testData)) {
-            array.push(value.name);
+    const handleChange = () => (event) => {
+        setDetail(event.target.value);
+    }
+
+    const handleAdd = () => (event) => {
+        const _id = details.length;
+        const parseDetail = {
+            id: _id,
+            name: detail,
+            items: [],
         }
-     
-        return (
-            <div>
-                { array.map((item) => <ListItem>{item}</ListItem> )}
-            </div>
-        );
+        const newDetails = details.concat(parseDetail);
+
+        setDetails(newDetails);
     }
 
-
+    const test = () => {
+        console.log(details);
+    }
+    
     return (
         <Container>
             <Card>
+                <Button onClick={test}>Test Details</Button>
                 <Typography>
                     Details
                 </Typography>
-                <Button>Add</Button>
-                <Button></Button>
+                
                 <List>
-                    {/* <ListItem button={true}>Item 1</ListItem>
-                    <ListItem>Item 2</ListItem>
-                    <ListItem>Item 3</ListItem>
-                    <ListItem>Item 4</ListItem>
-                    <ListItem>Item 5</ListItem> */}
-                    { parseData() }
+                    { details.map((item) => (
+                        <ListItem>{item.name}</ListItem>
+                    )) }
                 </List>
+
+                <div>
+                    <Button onClick={handleAdd()}>Add</Button>
+                    <Input 
+                        type="text"
+                        value={detail}
+                        onChange={handleChange()}
+                    />
+                </div>
+                
             </Card>
         </Container>
     )
